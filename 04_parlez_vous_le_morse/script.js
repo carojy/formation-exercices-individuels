b307006d24d5c09ba945606303c3a0020fce8b4c
caro <cirier.c@gmail.com>
2024-03-29 10:05:59 +0100

    ajout exo4

---
+++ 04_parlez_vous_le_morse/script.js
@@ -0,0 +1,38 @@
+function getLatinCharacterList(element){
+    let separation = element.split("")
+    return separation
+}
+
+console.log(getLatinCharacterList("Hello World"))
+
+
+function translateLatinCharacter(){
+    const latinToMorse = {
+        'A':'.-',
+        'B':'-...',
+        'C':'-.-.',
+        'D':'-..',
+        'E':'.',
+        'F':'..-.',
+        'G':'--.',
+        'H':'....',
+        'I':'..',
+        'J':'.---',
+        'K':'-.-',
+        'L':'.-..',
+        'M':'--',
+        'N':'-.',
+        'O':'---',
+        'P':'.--.',
+        'Q':'--.-',
+        'R':'.-.',
+        'S':'...',
+        'T':'-',
+        'U':'..-',
+        'V':'...-',
+        'W':'.--',
+        'X':'-..-',
+        'Y':'-.--',
+        'Z':'--..'
+    }
+}
