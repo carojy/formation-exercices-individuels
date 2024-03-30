e67c58336000b4d09703a046a8d31ca89c8f8e37
caro <cirier.c@gmail.com>
2024-03-30 15:02:45 +0100

    etape 4

--- 04_parlez_vous_le_morse/script.js
+++ 04_parlez_vous_le_morse/script.js
@@ -1,3 +1,5 @@
+// Etape 1 
+
 function getLatinCharacterList(element){
     let separation = element.split("")
     return separation
@@ -5,8 +7,8 @@ function getLatinCharacterList(element){
 
 console.log(getLatinCharacterList("Hello World"))
 
-
-function translateLatinCharacter(){
+// Etape 2
+function translateLatinCharacter(lettre){
     const latinToMorse = {
         'A':'.-',
         'B':'-...',
@@ -35,4 +37,102 @@ function translateLatinCharacter(){
         'Y':'-.--',
         'Z':'--..'
     }
+
+    const tableaux = Object.entries(latinToMorse)   // Object.entries() renvoie un tableau pour chaque ligne d'un objet 
+
+    for (i=0 ; i<tableaux.length ; i++ ){
+
+        if (lettre == tableaux[i][0]){
+            return tableaux[i][1]
+        }
+    }
+
 }
+
+//console.log(translateLatinCharacter("K"))
+
+// Etape 3
+function encode(texte){
+let majuscule = texte.toUpperCase()
+let variable = getLatinCharacterList(majuscule)
+let traduction = []
+
+for (let i=0 ; i<variable.length ; i++){
+    traduction[i]=translateLatinCharacter(variable[i])
+    }
+return traduction
+}
+
+encode("coucou petit cul")
+console.log(encode("coucou petit cul"))
+
+
+
+
+// Etape 4
+
+function getMorseList(element){
+    let ciseaux = element.split(" ")
+    return ciseaux
+}
+
+function translateMorseCharacter(lettre){
+    const morseToLatin = {
+        '-': "T",
+        '--': "M",
+        '---': "O",
+        '--.': "G",
+        '--.-': "Q",
+        '--..': "Z",
+        '-.': "N",
+        '-.-': "K",
+        '-.--': "Y",
+        '-.-.': "C",
+        '-..': "D",
+        '-..-': "X",
+        '-...': "B",
+        '.': "E",
+        '.-': "A",
+        '.--': "W",
+        '.---': "J",
+        '.--.': "P",
+        '.-.': "R",
+        '.-..': "L",
+        '..': "I",
+        '..-': "U",
+        '..-.': "F",
+        '...': "S",
+        '...-': "V",
+        '....': "H"
+      }
+    
+const tableaux2 = Object.entries(morseToLatin)   // Object.entries() renvoie un tableau pour chaque ligne d'un objet 
+
+for (i=0 ; i<tableaux2.length ; i++ ){
+
+    if (lettre == tableaux2[i][0]){
+        return tableaux2[i][1]
+    }
+}
+
+}
+//console.log(translateMorseCharacter("...-"))
+
+function decode(morse){
+ 
+    let variable = getMorseList(morse)
+
+    let traduction = []
+    
+    for (let i=0 ; i<variable.length ; i++){
+        traduction[i]=translateMorseCharacter(variable[i])
+        }
+    return traduction.toString()
+    }
+    
+    decode('-.-. --- ..- -.-. --- ..- .--. . - .. - -.-. ..- .-..')
+    console.log(decode('-.-. --- ..- -.-. --- ..- .--. . - .. - -.-. ..- .-..'))
+
+    // To do : afficher le string sans virgules
+
+
