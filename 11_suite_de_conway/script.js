cee8882a76b77d3d9e0119329338b84a02410ca5
caro <cirier.c@gmail.com>
2024-10-09 12:57:52 +0200

    maj suite de conway

--- 11_suite_de_conway/script.js
+++ 11_suite_de_conway/script.js
@@ -16,3 +16,21 @@ function decoupeChaine(string){
   
   console.log(decoupeChaine("aaabbbccdddefghh"))
   
+
+//Renvoie une string qui décrit les caractères qui constituent la chaîne en paramètre.
+
+function decritChaine(stringADecrire){
+    let result = ""
+    
+    for (let i = 1 ; i < stringADecrire.length + 1 ; i++){
+      if (stringADecrire[i] === stringADecrire[i-1]){
+        result += stringADecrire[i]
+      }else{
+        result += stringADecrire[i-1] + " "
+      }
+    }
+    
+    return result
+}
+
+console.log(decritChaine("aabbca"))
